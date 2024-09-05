1af6e67e0e47022fe78dd8380526b5e3d0474e86
Joseph T. Reginaldo <joseph.reginaldo@ymail.com>
2024-09-05 16:25:02 +0800

    ncr

---
+++ MgmntSystem 2024_05_21/2daysReminder/reminder.bat
@@ -0,0 +1,8 @@
+@echo off
+
+"C:\Users\ji26842035\AppData\Local\Programs\Spyder\Python\python.exe" "C:\Users\ji26842035\MgmntSystem 2024_05_21\2daysReminder\reminder.py"
+
+Rem "C:\Users\ji26842035\AppData\Local\Programs\Python\Python36\python.exe" "C:\Users\ji26842035\MgmntSystem 2024_05_21\2daysReminder\reminder.py"
+REM python -c "import automated;automated.update_date()" > C:\Users\ec.marinas\BatchTestEdric\lists.txt
+
+pause
